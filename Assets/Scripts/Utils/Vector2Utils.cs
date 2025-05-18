3ffe1cac26bb248d186e73e79e789337e3f51fa8
Ellandq <waselland@gmail.com>
2025-05-18 13:42:31 +0200

    Added Vector2 utils

---
+++ Assets/Scripts/Utils/Vector2Utils.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Characters.Movement;
+using UnityEngine;
+
+namespace Utils
+{
+    public static class Vector2Utils
+    {
+        public static Vector2 convertFromDirections(Dictionary<MoveDirection, bool> directions)
+        {
+            var result = Vector2.zero;
+
+            if (directions[MoveDirection.Up]) result += Vector2.up;
+            if (directions[MoveDirection.Down]) result += Vector2.down;
+            if (directions[MoveDirection.Left]) result += Vector2.left;
+            if (directions[MoveDirection.Right]) result += Vector2.right;
+
+            return result.normalized;
+        }
+    }
+}
