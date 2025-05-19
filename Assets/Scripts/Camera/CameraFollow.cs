d1f0360a74fe6cec81ac50945c0fef41a79d5b52
Ellandq <waselland@gmail.com>
2025-05-19 21:32:17 +0200

    Implemented camera movement

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Characters;
+using UnityEngine;
+
+namespace Camera
+{
+    public class CameraFollow : MonoBehaviour
+    {
+        [Header("Object References")] 
+        [SerializeField] private Player player;
+
+        [Header("Movement Settings")] 
+        [SerializeField] private float speed = 5f;
+
+        private void LateUpdate()
+        {
+            var playerPos = player.transform.position;
+            playerPos.z = transform.position.z;
+            var newPos = Vector3.Lerp(transform.position, playerPos, speed * Time.deltaTime);
+            transform.position = newPos;
+        }
+    }
+}
