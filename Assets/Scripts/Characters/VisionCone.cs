a751c3e40d36b47be7969aabc56b0000e7516836
Ellandq <waselland@gmail.com>
2025-05-20 21:44:28 +0200

    Added view cone lighting

--- Assets/Scripts/Characters/VisionCone.cs
+++ Assets/Scripts/Characters/VisionCone.cs
@@ -2,16 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 namespace Characters
 {
     [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+    [RequireComponent(typeof(Light2D))]
     public class VisionCone : MonoBehaviour
     {
         [Header("Vision Cone Settings")]
         [SerializeField] private float visionDistance = 3f;
         [SerializeField] private float visionAngle = 90f;
-        [SerializeField] private int rayCount = 50;
+        [SerializeField] private int rayCount = 90;
         [SerializeField] private LayerMask obstacleMask;
         [SerializeField] private LayerMask targetMask;
         private float _currentRotation;
@@ -23,24 +25,35 @@ namespace Characters
         [SerializeField] private MeshFilter meshFilter;
         [SerializeField] private MeshRenderer meshRenderer;
         private Mesh _visionMesh;
+        private Light2D _flashlight;
 
-        [Header("Materials")] 
+        [Header("Materials")]
         [SerializeField] private Material chaseMat;
         [SerializeField] private Material alertMat;
         [SerializeField] private Material normalMat;
 
         public void Initialize(Action onPlayerFound, Action onPlayerLost)
         {
-            _visionMesh = new Mesh
-            {
-                name = "Vision Mesh"
-            };
+            _visionMesh = new Mesh { name = "Vision Mesh" };
             meshFilter.mesh = _visionMesh;
             meshRenderer.sortingLayerName = "Midground";
             meshRenderer.sortingOrder = 1;
             _onPlayerFound = onPlayerFound;
             _onPlayerLost = onPlayerLost;
             _canSeePlayer = false;
+        }
+
+        void Awake()
+        {
+            if (!meshFilter) meshFilter = GetComponent<MeshFilter>();
+            if (!meshRenderer) meshRenderer = GetComponent<MeshRenderer>();
+            _flashlight = GetComponent<Light2D>();
+            _flashlight.lightType = Light2D.LightType.Freeform;
+            _flashlight.intensity = 1f;
+            _flashlight.pointLightOuterRadius = visionDistance;
+            _flashlight.shadowIntensity = 0f;
+            _flashlight.color = Color.white;
+            _flashlight.falloffIntensity = 1f;
         }
 
         public void UpdateVisionCone(float rotation)
@@ -50,7 +63,7 @@ namespace Characters
                 _visionMesh = new Mesh { name = "Vision Mesh" };
                 meshFilter.mesh = _visionMesh;
             }
-            
+
             _currentRotation = rotation;
             var startAngle = _currentRotation - visionAngle / 2f;
             var angleStep = visionAngle / rayCount;
@@ -58,27 +71,18 @@ namespace Characters
             var vertices = new List<Vector3> { Vector3.zero };
             var triangles = new List<int>();
             var worldPoints = new List<Vector2>();
+            var localPoints = new List<Vector2>();
 
             for (var i = 0; i <= rayCount; i++)
             {
                 var angle = startAngle + angleStep * i;
                 var dir = DirFromAngle(angle);
                 var hit = Physics2D.Raycast(transform.position, dir, visionDistance, obstacleMask);
-                Vector2 endpoint;
-
-                if (hit.collider)
-                {
-                    endpoint = hit.point;
-                }
-                else
-                {
-                    endpoint = (Vector2)transform.position + dir * visionDistance;
-                }
+                var endpoint = hit.collider ? hit.point : (Vector2)transform.position + dir * visionDistance;
 
                 worldPoints.Add(endpoint);
-
-                var localVertex = transform.InverseTransformPoint(endpoint);
-                vertices.Add(localVertex);
+                localPoints.Add(transform.InverseTransformPoint(endpoint));
+                vertices.Add(transform.InverseTransformPoint(endpoint));
             }
 
             for (var i = 1; i < vertices.Count - 1; i++)
@@ -87,14 +91,22 @@ namespace Characters
                 triangles.Add(i);
                 triangles.Add(i + 1);
             }
-            
+
             _visionMesh.Clear();
             _visionMesh.vertices = vertices.ToArray();
             _visionMesh.triangles = triangles.ToArray();
             _visionMesh.RecalculateNormals();
 
+            var localPoints3D = new List<Vector3> { Vector3.zero };
+            localPoints3D.AddRange(localPoints.Select(p => new Vector3(p.x, p.y, 0f)));
+            localPoints3D.Add(Vector3.zero);
+
+            if (_flashlight != null)
+                _flashlight.SetShapePath(localPoints3D.ToArray());
+
             DetectTargets(worldPoints);
         }
+
 
         private Vector2 DirFromAngle(float angleInDegrees)
         {
@@ -145,6 +157,5 @@ namespace Characters
                     throw new ArgumentOutOfRangeException(nameof(state), state, null);
             }
         }
-
     }
 }
